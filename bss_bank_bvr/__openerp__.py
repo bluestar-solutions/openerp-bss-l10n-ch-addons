7de338c140c908c91aa0e9098648949f6ddf3767
Daniel Le Gall <daniel.legall@blues2.ch>
2015-04-09 16:05:21 +0200

    fix version

--- bss_bank_bvr/__openerp__.py
+++ bss_bank_bvr/__openerp__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Bank BVR',
-    'version': 'master',
+    'version': '7.0.2.2-20150409',
     "category" : 'Bluestar/Generic module',
     'complexity': "easy",
     'description': """
